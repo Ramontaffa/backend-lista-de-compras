6e10cab55750a09da17556d8d62c65c80d8b9a23
Ramontaffa <taffarelramon1@gmail.com>
2025-10-19 22:41:32 -0300

    fix: add missing CORS origin for new deployment URL

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,8 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'https://lista-de-compras-liart-iota.vercel.app/',
-    'https://lista-de-compras-git-feat-integrated-main-ramontaffas-projects.vercel.app'
+    'https://lista-de-compras-git-feat-integrated-main-ramontaffas-projects.vercel.app',
+    'https://vercel.com/ramontaffarelcesars-projects/lista-de-compras-integrada/drbtxQhbndKA8RbrYC5baUUcwJdd'
   ]
 }));
 
